a88d28e6f5b78fbb8cb0f939084067015c6b1757
CrossDvs <activityscuslt52i@outlook.com>
2024-11-27 19:44:08 -0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-pip3 install frida-tools -y
+pip3 install frida-tools 
 frida --v
 /usr/local/lib/android/sdk/platform-tools/adb -s emulator-5554 pull /apex/com.android.art/lib64/libart.so .
